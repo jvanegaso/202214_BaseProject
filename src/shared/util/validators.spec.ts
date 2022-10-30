b44e5321137791042916b79a5e3385191881e2c2
Juan Vanegas <j.vanegaso@uniandes.edu.co>
2022-10-29 21:11:39 -0500

    Add tests to increase coverage

--- src/shared/util/validators.spec.ts
+++ src/shared/util/validators.spec.ts
@@ -1,7 +1,16 @@
 import { validateEmail } from './validators';
 
 describe('validateEmail', () => {
-  it('should return false if not valid email', () => {
+  it('should return null if not valid email', () => {
     expect(validateEmail('bad-email')).toBe(null);
+  });
+
+  it('should return false if there is no argument', () => {
+    expect(validateEmail(null)).toBe(false);
+  });
+
+  it('should return an String object if a correct email is passed as argument', () => {
+    const result = validateEmail('test@test.com');
+    expect(result[0]).toBe('test@test.com');
   });
 });
