d9dabee5c73c1693ee00a73513d10cf439f94a38
Juan Sebastian Vanegas <j.vanegaso@uniandes.edu.co>
2022-11-03 09:41:22 -0500

    Add duplicated functions to test the quality gates

--- src/shared/util/validators.ts
+++ src/shared/util/validators.ts
@@ -9,6 +9,28 @@ export const validateEmail = (email) => {
     );
 };
 
+export const validateEmail1 = (email) => {
+  if (!email) {
+    return false;
+  }
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    );
+};
+
+export const validateEmail2 = (email) => {
+  if (!email) {
+    return false;
+  }
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    );
+};
+
 export const hasValidLength = (value, maxLength) => {
   if (typeof value !== 'string') {
     return false;
@@ -17,6 +39,21 @@ export const hasValidLength = (value, maxLength) => {
 };
 
 export const startsWith = (search, rawPos) => {
+  const pos = rawPos > 0 ? rawPos|0 : 0;
+  return this.substring(pos, pos + search.length) === search;
+};
+
+export const startsWith1 = (search, rawPos) => {
+  const pos = rawPos > 0 ? rawPos|0 : 0;
+  return this.substring(pos, pos + search.length) === search;
+};
+
+export const startsWith2 = (search, rawPos) => {
+  const pos = rawPos > 0 ? rawPos|0 : 0;
+  return this.substring(pos, pos + search.length) === search;
+};
+
+export const startsWith3 = (search, rawPos) => {
   const pos = rawPos > 0 ? rawPos|0 : 0;
   return this.substring(pos, pos + search.length) === search;
 };
