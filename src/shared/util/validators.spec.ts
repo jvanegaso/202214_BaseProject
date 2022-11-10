b1bfc1f40564fb6e74d85b4574572886796ce165
Andrés Cendales <andres01660@gmail.com>
2022-11-09 22:21:52 -0500

    feat: adding validators tests

--- src/shared/util/validators.spec.ts
+++ src/shared/util/validators.spec.ts
@@ -3,6 +3,12 @@ import {
   getRandomNumberInRange,
   falsyValidator,
   iterate,
+  hasValidLength,
+  removeDuplicates,
+  camelToSnakeCase,
+  findIndex,
+  WrongFunction,
+  mockExceptionFunction,
 } from './validators';
 
 describe('validateEmail', () => {
@@ -36,6 +42,16 @@ describe('getRandomInRange', () => {
     const randomValue = getRandomNumberInRange(0, undefined);
     expect(randomValue).toBeUndefined;
   });
+
+  it('shoud return if lower is undefined', () => {
+    const randomValue = getRandomNumberInRange(undefined, 10);
+    expect(randomValue).toBeUndefined;
+  });
+
+  it('shoud return if upper is undefined', () => {
+    const randomValue = getRandomNumberInRange(0, undefined);
+    expect(randomValue).toBeUndefined;
+  });
 });
 
 describe('falsyValidator', () => {
@@ -58,5 +74,80 @@ describe('iterate', () => {
 
   it('should return false if the argument is not an array', () => {
     expect(iterate(null)).toBeFalsy;
+  });
+});
+
+describe('hasValidLength', () => {
+  it('should return false if is not string', () => {
+    const result = hasValidLength(10, 10);
+    expect(result).toBeFalsy();
+  });
+
+  it('should return false if the string is longer than the max length', () => {
+    const result = hasValidLength('test', 2);
+    expect(result).toBeFalsy();
+  });
+
+  it('should return true if the string is shorter than the max length', () => {
+    const result = hasValidLength('test', 10);
+    expect(result).toBeTruthy();
+  });
+});
+
+describe('removeDuplicates', () => {
+  it('should return an array with no duplicates', () => {
+    const result = removeDuplicates([1, 2, 3, 1, 2, 3]);
+    expect(result).toEqual([1, 2, 3]);
+  });
+
+  it('should return undefined if the argument is not an array', () => {
+    const result = removeDuplicates(1);
+    expect(result).toBeUndefined;
+  });
+});
+
+describe('camelToSnakeCase', () => {
+  it('should return a snake case string', () => {
+    const result = camelToSnakeCase('testString');
+    expect(result).toBe('test_string');
+  });
+
+  it('should return the same string if the argument is not a string', () => {
+    const result = camelToSnakeCase(1);
+    expect(result).toBe(1);
+  });
+});
+
+describe('findIndex', () => {
+  it('should return the index of the element', () => {
+    const result = findIndex([1, 2, 3], function (element) {
+      return element === 2;
+    });
+    expect(result).toBe(1);
+  });
+
+  it('should return -1 if the element is not found', () => {
+    const result = findIndex([1, 1, 3], function (element) {
+      return false;
+    });
+    expect(result).toBeUndefined();
+  });
+});
+
+describe('wrong function', () => {
+  it('should return false if are diferent', () => {
+    const result = WrongFunction(1, 2);
+    expect(result).toBeFalsy();
+  });
+  it('should return true if are equal', () => {
+    const result = WrongFunction(1, 1);
+    expect(result).toBeTruthy();
+  });
+});
+
+describe('mockExceptionFunction', () => {
+  it('should return 3', () => {
+    const result = mockExceptionFunction([1, 2, 3]);
+    expect(result).toBe(3);
   });
 });
